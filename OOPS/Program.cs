354a86e3050b7edfcbd7021d4713626c8414b173
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-06-25 12:51:17 -0400

    Add project files.

---
+++ OOPS/Program.cs
@@ -0,0 +1,23 @@
+﻿student std = new student();
+std.marks = 10;
+std.name = "Sushant";
+std.age = 21;
+
+std.details();
+
+
+
+ class student { 
+  public  string name;
+    public int age;
+    public int marks;
+
+
+    // function
+    public void details()
+{
+        Console.WriteLine("Name= {0} Age = {1} Marks ={2}",name,age,marks);
+}
+
+}
+
